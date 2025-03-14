29653a6d7d9a46920fb35a04bf05f5ec44d3a1b8
JuandaMonkey <juandanieldecide@gmail.com>
2025-03-14 08:20:23 -0600

    feat(model): se agrego sliderbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule, MatSidenavModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'iron-revolution-center-app-web';
+  isOpen = true; 
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
 }
